8c9e5d14890a33e48ed697d6b9c57cfcf7a80fc8
ClaudiaIvanescu <claudiac92@yahoo.com>
2023-08-21 22:18:20 +0300

    first commit
    upload project for final exam

---
+++ src/main/itfactory/project/user_service/Main.java
@@ -0,0 +1,14 @@
+package main.itfactory.project.user_service;
+
+import main.itfactory.project.user_service.console_ui.MenuController;
+import main.itfactory.project.user_service.service.DatabaseInitialService;
+import main.itfactory.project.user_service.service.UserDao;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DatabaseInitialService.createMissingTable();
+        UserDao dao = new UserDao();
+        MenuController.showMenu(dao);
+    }
+}
---
+++ src/main/itfactory/project/user_service/console_ui/EditUserController.java
@@ -0,0 +1,24 @@
+package main.itfactory.project.user_service.console_ui;
+
+import main.itfactory.project.user_service.dto.User;
+
+public class EditUserController {
+    public static User readNewUser() {
+        System.out.println("Enter details to create new user : ");
+        String name = MenuUtil.readString("Name: ", false);
+        String surname = MenuUtil.readString("Surname: ", false);
+        String email = MenuUtil.readString("Email: ", false);
+        int age = MenuUtil.readInt("Age: ");
+        return new User(name, surname, email, age);
+    }
+
+    public static User readUserForUpdate(long id) {
+        System.out.println("Enter details to edit existing user with id = "+ id+" : ");
+        String name = MenuUtil.readString("Name: ", false);
+        String surname = MenuUtil.readString("Surname: ", false);
+        String email = MenuUtil.readString("Email: ", false);
+        int age = MenuUtil.readInt("Age: ");
+
+        return new User(id, name, surname, email, age);
+    }
+}
